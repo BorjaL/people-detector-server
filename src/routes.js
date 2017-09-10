72b0d156ce2f1b25f67f4d09a708fb1b1b33af3d
BorjaL <borja.fpillado@gmail.com>
2017-09-10 19:50:19 +0200

    Adding Api key authentication

--- src/routes.js
+++ src/routes.js
@@ -3,22 +3,24 @@ var prepareInfo = require('./prepare_info');
 var weatherInfo = require('./weather');
 var KeenTracking = require('keen-tracking');
 var router = express.Router();
+var passport = require('passport');
 
 var client = new KeenTracking({
   projectId: process.env.KEEN_PROJECT_ID,
   writeKey: process.env.KEEN_WRITE_KEY
 });
 
-router.get('/count', function(req, res) {
-  console.log('Save & sound');
+router.get('/count', passport.authenticate('localapikey', { session: false }),
+  function(req, res) {
+    console.log('Save & sound');
 
-  weatherInfo(function(weather_info){
-      var date =  new Date();
-      req.db.collection('detections').insertOne(prepareInfo(date, weather_info));
-      client.recordEvent('person_detected', prepareInfo(date, weather_info));
-  })
+    weatherInfo(function(weather_info){
+        var date =  new Date();
+        req.db.collection('detections').insertOne(prepareInfo(date, weather_info));
+        client.recordEvent('person_detected', prepareInfo(date, weather_info));
+    })
 
-  res.send();
+    res.send();
 });
 
 module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var expressMongoDb = require('express-mongo-db');
 var env = require('node-env-file');
+var passport = require('passport');
+var LocalAPIKeyStrategy = require('passport-localapikey').Strategy;
 
 env('.env');
 
 var app = express();
 app.use(expressMongoDb('mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/people_detector'));
-
-var port = process.env.PORT || 8080;
-
+app.use(passport.initialize());
 app.use('/', require('./routes'));
 
+passport.use(new LocalAPIKeyStrategy(
+  function(apikey, done) {
+    return done(null, apikey == process.env.API_KEY);
+  }
+));
+
+var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('Go to this port ' + port);
